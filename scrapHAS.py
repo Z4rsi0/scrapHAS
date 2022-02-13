49bbc289bf5378d52eec482de09b48ce473493e9
Z4rsi0 <89769065+Z4rsi0@users.noreply.github.com>
2022-02-13 12:31:28 +0100

    Update scrapHAS.py

--- scrapHAS.py
+++ scrapHAS.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.has-sante.fr/jcms/c_2608425/fr/ue-2-de-la-conception-a-la-naissance-pathologie-de-la-femme-heredite-l-enfant-l-adolescent'
-
+url = 'https://www.has-sante.fr/jcms/c_2608425/fr/ue-2-de-la-conception-a-la-naissance-pathologie-de-la-femme-heredite->
 response = requests.get(url)
 
 if response.ok:
     soup = BeautifulSoup(response.text, 'lxml')
-    for link in soup.select('div.encart div.wysiwyg ul li a[href]'):
-        print(link['href'])
+    for link in soup.select('div.encart div.wysiwyg ul li a[href]'):  #penser à faire pareil avec 'div.encart div.wysiwyg ul li u a[href]'
+        lien = str(link['href'])                    #sélection du lien
+        lien = lien.lstrip("https://has-sante.fr/") #uniformisation
+        print(lien)
